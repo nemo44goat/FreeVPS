9ccb72ee67a8f42ff27ab5748bb362260e679fbf
nemo44goat <happybasg@gmail.com>
2026-03-29 12:04:30 -0400

    Update macos-run.sh

--- macos-run.sh
+++ macos-run.sh
@@ -9,10 +9,16 @@ brew install --cask google-chrome
 # Install Chrome Remote Desktop
 curl -L "https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg" -o /tmp/crd.dmg
 sudo hdiutil attach /tmp/crd.dmg
-sudo installer -pkg "/Volumes/Chrome Remote Desktop/ChromeRemoteDesktop.pkg" -target /
-sudo hdiutil detach "/Volumes/Chrome Remote Desktop"
 
-# Run the auth command from secret
+# Volume adını avtomatik tap
+VOLUME=$(ls /Volumes | grep "Chrome Remote Desktop")
+echo "Volume tapildi: $VOLUME"
+sudo installer -pkg "/Volumes/$VOLUME/ChromeRemoteDesktop.pkg" -target /
+sudo hdiutil detach "/Volumes/$VOLUME"
+
+sleep 3
+
+# Chrome Remote Desktop-u işlat
 eval "$1"
 
 sleep 5
